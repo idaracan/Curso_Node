a88599f3bd2ab67aad4e3d04c4c8984329f31cb7
Ivan <ivandarioramirezcantillo@gmail.com>
2018-12-13 23:37:10 -0500

    updated clima route

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const argv = require('./config/yargs').argv
 const getLatLng = require('./lugar/lugar').getLatLng
-const getClima  = require('./lugar/clima').getClima
+const getClima  = require('./clima/clima').getClima
 
 getLatLng(argv.d).then().then(res => getClima(res)).then(res => console.log(res)).catch(error => console.log(error))
 
